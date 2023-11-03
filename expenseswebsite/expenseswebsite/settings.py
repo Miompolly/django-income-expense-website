9ef41397a30207c10d80160ddf802cdfb8a3d238
miompolly <57joel39@gmail.com>
2023-11-03 09:46:47 +0200

    connect with postgress

--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -77,14 +77,25 @@ WSGI_APPLICATION = 'expenseswebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.environ.get('DB_ENGINE'),
+#         'NAME':os.environ.get('DB_NAME'),
+#         'USER':os.environ.get('DB_USER'),
+#         'PASSWORD':os.environ.get('DB_USER_PASSWORD'),
+#         'HOST':os.environ.get('DB_HOST'),
+#         'PORT':os.environ.get('DB_PORT'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE'),
-        'NAME':os.environ.get('DB_NAME'),
-        'USER':os.environ.get('DB_USER'),
-        'PASSWORD':os.environ.get('DB_USER_PASSWORD'),
-        'HOST':os.environ.get('DB_HOST'),
-        'PORT':os.environ.get('DB_PORT'),
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':'incomeexpensesdb',
+        'USER':'postgres',
+        'PASSWORD':'Joel@123',
+        'HOST':'localhost',
+        'PORT':5432,
     }
 }
 
--- expenseswebsite/expenseswebsite/static/js/register.js
+++ expenseswebsite/expenseswebsite/static/js/register.js
@@ -1,20 +1,33 @@
-const usernameField=document.querySelector("#usernameField");
+const usernameField = document.querySelector("#usernameField");
 
-usernameField.addEventListener('keyup',(e)=>{
-    const usernameVal=e.target.value
-    console.log(usernameVal)
-    if(usernameVal.length>0){
-        fetch("/authentication/validate-username",{
-            body:JSON.stringify({username:usernameVal}),
-            method:"POST",
+usernameField.addEventListener('keyup', (e) => {
+    const usernameVal = e.target.value;
+    console.log(usernameVal);
+
+    if (usernameVal.length > 0) {
+        fetch("/authentication/validate-username", {
+            body: JSON.stringify({ username: usernameVal }),
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
         })
-        .then((res)=>res.json())
-        .then((data)=>{
-            console.log("data",data);
-            if(data.username_error){
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return res.json();
+        })
+        .then((data) => {
+            console.log("data", data);
+            if (data.username_error) {
                 usernameField.classList.add("is-invalid");
             }
         })
+        .catch((error) => {
+            console.error('Fetch error:', error);
+        });
+        
 
     }
-})
+});
