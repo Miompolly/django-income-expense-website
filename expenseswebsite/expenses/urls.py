cd4f74a9bc507bf54311d15b55ef698ebb6aa3bf
miompolly <57joel39@gmail.com>
2023-11-08 18:45:45 +0200

    export excel csv pdf

--- expenseswebsite/expenses/urls.py
+++ expenseswebsite/expenses/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.index, name='expenses'),
     path('add-expense', views.add_expense, name='add-expenses'),
     path('edit-expense/<int:id>', views.expense_edit, name='expense-edit'),
-    path('expense-delete/<int:id>', views.delete_expense, name='expense-delete')
+    path('expense-delete/<int:id>', views.delete_expense, name='expense-delete'),
+    path('export-to-csv/', views.export_to_csv, name='export-to-csv'),
+    path('export-to-excel/', views.export_to_excel, name='export-to-excel'),
+    path('export-to-pdf/', views.export_to_pdf, name='export-to-pdf'),
 ]
--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -4,9 +4,16 @@ from .models import Category,Expense
 from django.contrib import messages
 from django.shortcuts import redirect
 from django.core.paginator import Paginator
+import csv
+from django.http import HttpResponse
+from io import BytesIO
+from reportlab.pdfgen import canvas
+from django.http import HttpResponse
+import openpyxl
+from openpyxl.utils import get_column_letter
 
 
-# Create your views here.
+
 @login_required(login_url='/authentication/login')
 def index(request):
     categories=Category.objects.all()
@@ -94,6 +101,79 @@ def delete_expense(request,id):
     expense.delete()
     messages.success(request,"Expense removed successfully")
     return redirect("expenses")
+
+@login_required(login_url='/authentication/login')
+def export_to_excel(request):
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="expenses.xlsx"'
+
+    expenses = Expense.objects.filter(owner=request.user)  # Adjust the query as needed
+
+    workbook = openpyxl.Workbook()
+    worksheet = workbook.active
+
+    worksheet['A1'] = 'Date'
+    worksheet['B1'] = 'Amount'
+    worksheet['C1'] = 'Category'
+    worksheet['D1'] = 'Description'
+
+    for row_num, expense in enumerate(expenses, start=2):
+        worksheet.cell(row=row_num, column=1, value=expense.date)
+        worksheet.cell(row=row_num, column=2, value=expense.amount)
+        worksheet.cell(row=row_num, column=3, value=expense.category)
+        worksheet.cell(row=row_num, column=4, value=expense.description)
+
+    workbook.save(response)
+
+    return response
+
+@login_required(login_url='/authentication/login')
+def export_to_pdf(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="expenses.pdf"'
+
+    expenses = Expense.objects.filter(owner=request.user)  
+
+    buffer = BytesIO()
+    p = canvas.Canvas(buffer)
+
+    p.drawString(100, 750, 'Date')
+    p.drawString(200, 750, 'Amount')
+    p.drawString(300, 750, 'Category')
+    p.drawString(400, 750, 'Description')
+
+    y = 730
+    for expense in expenses:
+        y -= 20
+        p.drawString(100, y, str(expense.date))
+        p.drawString(200, y, str(expense.amount))
+        p.drawString(300, y, expense.category)
+        p.drawString(400, y, expense.description)
+
+    p.showPage()
+    p.save()
+
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+
+    return response
+
+
+@login_required(login_url='/authentication/login')
+def export_to_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="expenses.csv"'
+
+    expenses = Expense.objects.filter(owner=request.user)  # Adjust the query as needed
+
+    writer = csv.writer(response)
+    writer.writerow(['Date', 'Amount', 'Category', 'Description'])
+
+    for expense in expenses:
+        writer.writerow([expense.date, expense.amount, expense.category, expense.description])
+
+    return response
       
 
 
